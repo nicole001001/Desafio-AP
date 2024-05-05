6586c7fdcb19176e6741509ee7a23f34ff411fbc
Nicole <nicoleururi@gmail.com>
2024-05-05 00:28:43 -0300

    Desafio 2

---
+++ Desafio 2/main.js
@@ -0,0 +1,11 @@
+const NombreProducto = "Máscara de Pestañas"
+const PrecioUnitario = 14802
+
+let CantidadDeseada = prompt(`Ingrese la cantidad de ${NombreProducto} que desee ૮ ․ ․ ྀིა : `)
+parseInt(CantidadDeseada);
+
+const Costototal = CantidadDeseada * PrecioUnitario
+
+alert (`Usted compro ${CantidadDeseada} ${NombreProducto} . El costo total es ✮⋆˙ദ്ദി ( ᵔ ᗜ ᵔ ): $${Costototal}`)
+
+
